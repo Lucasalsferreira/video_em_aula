1f0ca8038194f8f383bdcfcb10fd15593c5af1b7
lucasalsferreira <lucas_araujo.20@live.com>
2020-09-21 17:48:34 -0300

    comecamos mais um desafio

---
+++ PythonDesafios/desafio003.py
@@ -0,0 +1,4 @@
+n1 = int(input('Me fale um número: '))
+n2 = int(input('Me fale mais um número: '))
+s = n1 + n2
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
---
+++ PythonDesafios/desafio004.py
@@ -0,0 +1,7 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é?', type(a))
+print('Só tem espaços?',a.isspace())
+print('Só tem letras?',a.isalpha())
+print('Só tem números?',a.isnumeric())
+print('Tem letras maiúsculas?',a.isupper())
+print('É alfanumérico?',a.isalnum())
